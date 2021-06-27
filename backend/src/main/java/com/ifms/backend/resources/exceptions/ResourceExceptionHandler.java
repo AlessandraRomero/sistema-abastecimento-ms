c0db42367f6013f175b0166e8bd85f8c538ee1e1
AlessandraRomero <alessandraromeiro098@gmail.com>
2021-06-27 17:24:59 -0400

    Implementando crud de abastecimento

--- backend/src/main/java/com/ifms/backend/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/ifms/backend/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.ifms.backend.services.exceptions.DataBaseException;
 import com.ifms.backend.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -25,4 +26,17 @@ public class ResourceExceptionHandler {
 		error.setPath(request.getRequestURI());
 		return ResponseEntity.status(status).body(error);
 	}
+	
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<StandardError> dataBase(DataBaseException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError error = new StandardError();
+		error.setTimestamp(Instant.now());
+		error.setStatus(status.value());
+		error.setError("Database exception");
+		error.setMessage(e.getMessage());
+		error.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(error);
+	}
+
 }
--- backend/src/main/java/com/ifms/backend/services/AbastecimentoService.java
+++ backend/src/main/java/com/ifms/backend/services/AbastecimentoService.java
@@ -5,7 +5,11 @@ import java.util.Optional;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.ifms.backend.dto.AbastecimentoDto;
 import com.ifms.backend.entities.Abastecimento;
 import com.ifms.backend.repositories.AbastecimentoRepository;
+import com.ifms.backend.services.exceptions.DataBaseException;
 import com.ifms.backend.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,9 +35,48 @@ public class AbastecimentoService {
 	@Transactional(readOnly = true)
 	public AbastecimentoDto findById(Long id) {
 		Optional<Abastecimento> obj = repository.findById(id);
-		Abastecimento abastecimento = obj.orElseThrow(() -> new ResourceNotFoundException("O abastecimento solicitado não foi encontrado"));
+		Abastecimento abastecimento = obj
+				.orElseThrow(() -> new ResourceNotFoundException("O abastecimento solicitado não foi encontrado"));
 		return new AbastecimentoDto(abastecimento);
 	}
 
+	@Transactional
+	public AbastecimentoDto insert(AbastecimentoDto dto) {
+		Abastecimento entity = new Abastecimento();
+		entity.setCpfMotorista(dto.getCpfMotorista());
+		entity.setDataDoAbastecimento(dto.getDataDoAbastecimento());
+		entity.setQuilometragem(dto.getQuilometragem());
+		entity.setCombustivel(dto.getCombustivel());
+		entity.setQuantidadeEmLitros(dto.getQuantidadeEmLitros());
+		entity.setValorPorLitro(dto.getValorPorLitro());
+		entity = repository.save(entity);
+		return new AbastecimentoDto(entity);
+	}
 
+	@Transactional
+	public AbastecimentoDto update(Long id, AbastecimentoDto dto) {
+		try {
+			Abastecimento entity = repository.getOne(id);
+			entity.setCpfMotorista(dto.getCpfMotorista());
+			entity.setDataDoAbastecimento(dto.getDataDoAbastecimento());
+			entity.setQuilometragem(dto.getQuilometragem());
+			entity.setCombustivel(dto.getCombustivel());
+			entity.setQuantidadeEmLitros(dto.getQuantidadeEmLitros());
+			entity.setValorPorLitro(dto.getValorPorLitro());
+			entity = repository.save(entity);
+			return new AbastecimentoDto(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("O id do abastecimento não foi localizado");
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Não foi possível deletar, id do abastecimento não foi localizado");
+		}catch (DataIntegrityViolationException e) {
+			throw new DataBaseException("Não foi possível deletar o abastecimento pois está em uso");
+		}
+	}
 }
